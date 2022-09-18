946f80a0ad17272fb7323d967d1290d7bf23595f
Thibaud Marin <thibaud.marin1@gmail.com>
2022-09-18 23:17:02 +0200

    Refonte visuel, voir pourquoi je ne gagne jamais

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,9 @@
 // Je declare mes variables et demande le nom du joueur humain 
+const stone = document.querySelector('buttonStone');
+const leaf = document.querySelector('buttonLeaf');
+const scissor = document.querySelector('buttonScissor')
 const buttons = document.querySelectorAll("button");
-const username = prompt("Veuillez indiquer votre nom d'utilisateur...");
+const username = prompt("Vous allez commencer une partie de shifumi ! Le premier a dix point gagne, bonne chance ! Veuillez indiquer votre nom d'utilisateur...");
 var userScoreDisplay = document.getElementById('userScore');
 var botScoreDisplay = document.getElementById('botScore');
 var userWin = 0;
@@ -15,7 +18,6 @@ document.onclick = function() {
 // Ici je recupere le choix du joueurs humain au click sur un bouton et je donne une valeur random au bot avec Math.floor et math.random
 
 for (let i = 0; i < buttons.length; i++){
-    // sound.play();
     buttons[i].addEventListener("click", function() {
     var joueur = buttons[i].innerHTML;
     var robot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML;
@@ -26,33 +28,34 @@ for (let i = 0; i < buttons.length; i++){
         resultat = "Vous avez fait une egalité !";
     } 
     else if((joueur === "Pierre" && robot === "Ciseaux") || (joueur === "Feuille" && robot === "Pierre") || (joueur === "Ciseaux" && robot === "Feuille")){
-        resultat = " 素晴らしい ! Tu as gagné !!";
+        resultat = "Tu as gagné !!";
         userWin++;
     } 
     else {
-        resultat = "BA-BAKA ! Vous avez perdu !";
+        resultat = "Vous avez perdu !";
         botWin++;
     }
 
 
     document.querySelector(".resultat").innerHTML = `
     ${username} a choisi : ${joueur}</br>
-    La Waifu a choisi : ${robot}</br>
+    l'adversaire a choisi : ${robot}</br>
     ${resultat}
     `;
     userScoreDisplay.innerHTML=userWin;
     botScoreDisplay.innerHTML=botWin;
 
     if(userWin === 10){
-        alert(`O/ UwU O/ Bravo ${username} tu remportes la victoire et tu rentre chez toi avec une encyclopedie en 78 volumes des maladies de l'oreille interne! `);
+        alert(` Bravo ${username} tu remportes la victoire ! La chance est de ton coté. Clique sur OK pour rejouer !`);
     } 
     else if (botWin === 10){
-        alert((`B-BA-BAKKA, j'ai voulu te laisser gagner, mais t'es trop un bakka !`));
+        alert((`Pas de chance cette fois-ci ! l'adversaire remporte la victoire. Clique sur OK pour rejouer !`));
     }
 
     if(userWin === 10 || botWin === 10){
-        location.reload();
         sound.play();
+        location.reload();
+        
     }
 
 })
